9078bcfc826fe2695449c81e34a4b1964c308739
GilDev <gildev@gmail.com>
2015-05-02 00:39:32 +0200

    Amélioration de l'interface d'administration

--- app/actions/admin/index.php
+++ app/actions/admin/index.php
@@ -2,5 +2,5 @@
 
 $this->needed('administrator');
 
-$stmt = $this['db']->query('SELECT id, prenom, nom, nb_coups_restants, vie, a_soigne FROM eleves WHERE id <> ' . $this['session.id'] . ' ORDER BY prenom');
+$stmt = $this['db']->query('SELECT id, prenom, nom, nb_coups_restants, vie, a_soigne FROM eleves ORDER BY prenom');
 $eleves = $stmt->fetchAll();
