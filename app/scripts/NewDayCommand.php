a9e69257361d7c72ee0e893e49677fbc36b7f912
GilDev <gildev@gmail.com>
2015-05-06 00:04:52 +0200

    Correction de bugs et amélioration des dieux

--- app/scripts/NewDayCommand.php
+++ app/scripts/NewDayCommand.php
@@ -32,16 +32,28 @@ class NewDayCommand extends ConsoleKit\Command
 		$victime  = Atomik::get('db')->selectOne('eleves', 'vie > 0 ORDER BY RANDOM()');
 		$nbCoups = mt_rand(1, 3);
 		$victime['vie'] -= $nbCoups;
+		if ($victime['vie'] < 0) {
+			$victime['vie'] = 0;
+		}
 		Atomik::get('db')->update('eleves', array('vie' => $victime['vie']), array('id' => $victime['id']));
 
 		$infosDieux = ($dieu[1]) ? 'Le dieu ' : 'La déesse <b>';
 		$infosDieux .= $dieu[0];
-		$infosDieux .= '</b> a aujourd\'hui choisi de victimiser <b>' . Atomik::formatName($victime) . '</b> et l\'a frappé <b>' . $nbCoups . '</b> fois !';
+		$infosDieux .= '</b> a aujourd\'hui choisi de victimiser <b>' . Atomik::formatName($victime) . '</b> et l\'a frappé <b>' . $nbCoups . '</b> fois';
+		if ($victime['vie'] <= 0) {
+			$infosDieux .= ', le menant à sa mort…';
+		} else {
+			$infosDieux .= ' !';
+		}
 
 
-		$chanceux = Atomik::get('db')->selectOne('eleves', 'vie > 0 AND vie < 10 AND id <> ' . $victime['id'] . ' ORDER BY RANDOM()');
+		$nbSoins = mt_rand(1, 2);
+		$chanceux = Atomik::get('db')->selectOne('eleves', 'vie > 0 AND vie <= ' . (10 - $nbSoins) . ' AND id <> ' . $victime['id'] . ' ORDER BY RANDOM()');
+		if ($chanceux == NULL && $nbSoins == 2) { // S'il n'y a pas d'élève 8 ou moins points de vie, on cherche parmis ceux qui ont 9 points de vie
+			$nbSoins = 1;
+			$chanceux = Atomik::get('db')->selectOne('eleves', 'vie > 0 AND vie <= 9 AND id <> ' . $victime['id'] . ' ORDER BY RANDOM()');
+		}
 		if ($chanceux != NULL) {
-			$nbSoins = mt_rand(1, 2);
 			$chanceux['vie'] += $nbSoins;
 			Atomik::get('db')->update('eleves', array('vie' => $chanceux['vie']), array('id' => $chanceux['id']));
 			$infosDieux .= "\n" . 'À cause d\'un faux jour sur l\'écran, le dieu <b>Pascal</b> a soigné <b>' . $nbSoins . '</b> fois <b>' . Atomik::formatName($chanceux) . '</b> !';
