9a7fb8a888c5e99a2d941439feb637d86aa80ea9
GilDev <gildev@gmail.com>
2015-05-15 13:11:07 +0200

    Compatibilité avec les noms ou prénoms comportant un tiret

--- app/helpers/formatName.php
+++ app/helpers/formatName.php
@@ -2,5 +2,14 @@
 
 function formatName($eleve)
 {
-	return ucwords($eleve['prenom']) . ' ' . ucwords($eleve['nom']);
+	if ($pos = strpos($eleve['prenom'], '-') !== false) {
+		$pos++;
+		$eleve['prenom'][$pos] = mb_convert_case($eleve['prenom'][$pos], MB_CASE_UPPER, 'UTF-8');
+	}
+	if ($pos = strpos($eleve['nom'], '-') !== false) {
+		$pos++;
+		$eleve['nom'][$pos] = mb_convert_case($eleve['nom'][$pos], MB_CASE_UPPER, 'UTF-8');
+	}
+
+	return mb_convert_case($eleve['prenom'], MB_CASE_TITLE, 'UTF-8') . ' ' . mb_convert_case($eleve['nom'], MB_CASE_TITLE, 'UTF-8');
 }
